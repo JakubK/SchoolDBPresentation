3c4ad8325abe72741e4a458ddec0ba37fe35c4d2
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-05-18 10:41:11 +0200

    Add incrementing animation of data and add next slide with total rows

---
+++ js/index.js
@@ -0,0 +1,54 @@
+var TotalRecords = 0;
+var elementMap = new Map();
+var textNodes = document.querySelectorAll("#data-numbers > p >span");
+for(var j = 0;j < textNodes.length;j++)
+{
+    elementMap.set(j, textNodes[j].textContent);
+    TotalRecords += parseInt(textNodes[j].textContent);
+}
+
+var completedRowCalulations = false;
+var i;
+function IncrementAnimation(elements)
+{       
+        if(!completedRowCalulations)
+        {
+            i = 0;
+            var count = elements.length;
+            var element = elements[i];
+            var targetVal = element.textContent;
+            for(var j = 0;j < elements.length;j++)
+            {
+                elements[j].textContent = "0";
+            }
+        }
+        completedRowCalulations = true;
+        if(i == 0)
+        {
+        var interval = setInterval(function()
+        {
+            if(completedRowCalulations && i == count)
+                clearInterval(interval);
+
+            element.textContent = parseInt(element.textContent) + 1;
+            if(parseInt(element.textContent) >= targetVal)
+            {
+                i++;
+                if(i >= count)
+                {
+                    clearInterval(interval);
+                }
+                else //switch to next element
+                {
+                    element = elements[i];
+                    targetVal = elementMap.get(i);
+                    element.textContent = "0";
+                }
+            }
+        },30,element,targetVal);
+    }
+}
+
+
+
+
